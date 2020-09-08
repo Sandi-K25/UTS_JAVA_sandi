ea8032ae9d317361a89592d3037660bac62ef5f1
Sandi-K255 <Sandikalebali@gmail.com>
2020-09-08 11:49:37 +0700

    first commit

---
+++ UTS_SANDIKALEBALI_XIIRPLB.java
@@ -0,0 +1,37 @@
+package UTS;
+
+public class UTS_SANDIKALEBALI_XIIRPLB {
+	
+	public static void main(String[] args) {
+		String huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+		
+		int a1 = huruf.indexOf('S');
+		int a2 = huruf.indexOf('A');
+		int a3= huruf.indexOf('N');
+		int a4= huruf.indexOf('D');
+		int a5= huruf.indexOf('I');
+		int a6= huruf.indexOf('-');
+		int a7= huruf.indexOf('K');
+		int a8= huruf.indexOf('A');
+		int a9= huruf.indexOf('L');
+		int a10= huruf.indexOf('E');
+		int a11= huruf.indexOf('B'); 
+		
+		char h1 = huruf.charAt(a1);
+		char h2 = huruf.charAt(a2);
+		char h3 = huruf.charAt(a3);
+		char h4 = huruf.charAt(a4);
+		char h5 = huruf.charAt(a5);
+		char h6 = huruf.charAt(a6);
+		char h7 = huruf.charAt(a7);
+		char h8 = huruf.charAt(a8);
+		char h9 = huruf.charAt(a9);
+		char h10 = huruf.charAt(a10);
+		char h11 = huruf.charAt(a11);
+		
+		char nama[]= {h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11};
+		
+		System.out.println(nama);
+	}
+
+}

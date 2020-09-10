25a37c5fe63d2b03511f935a0493b933844c0d8c
Sandi-K255 <Sandikalebali@gmail.com>
2020-09-10 11:42:47 +0700

    first commit

---
+++ Agunguts.java
@@ -0,0 +1,30 @@
+package agung;
+
+public class Agunguts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String huruf="ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+				char resulta=huruf.charAt(0);
+				char resultb=huruf.charAt(6);
+				char resultc=huruf.charAt(20);
+				char resultd=huruf.charAt(13);
+				char resulte=huruf.charAt(6);
+				
+				char resultspasi=huruf.charAt(27);
+				
+				char resultf=huruf.charAt(6);
+				char resultg=huruf.charAt(20);
+				char resulth=huruf.charAt(13);
+				char resulti=huruf.charAt(19);
+				char resultj=huruf.charAt(0);
+				char resultk=huruf.charAt(17);
+				char resultl=huruf.charAt(0);
+				char arr[]= {resulta,resultb,resultc,resultd,resulte,resultspasi,resultf,resultg,resulth,resulti,resultj,resultk,resultl};
+				System.out.println(arr);
+	
+				
+		
+
+	}
+}
